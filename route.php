3ae5a6f3df7327000248cc9779c0c201053cbfa7
Robert Marianski <rob@marianski.com>
2010-08-13 16:31:28 -0400

    add routes and shapes for new fake bus route

--- route.php
+++ route.php
@@ -2,15 +2,27 @@
 
 $routeid = $_GET["routeId"];
 
-$route = array("routeId" => $routeid,
-               "polyline" => array(
+if ($routeid == "M14D") {
+  $polyline = array(
                    array(40.71841194933421,-74.0005588413086),
                    array(40.71785899481434,-73.99978636511231),
                    array(40.717078345319955,-73.9985418201294),
                    array(40.716200103696565,-73.99605273016358),
                    array(40.713923127013494,-73.99746893652345),
                    array(40.71353278033439,-73.99867056616212),
-                   ),
+                   );
+} elseif ($routeid == "M21C") {
+  $polyline = array(
+                   array(40.70998702652973,-73.99193285711671),
+                   array(40.71460622819519,-73.99283407934571),
+                   array(40.71912753071832,-73.99034498937989),
+                   );
+} else {
+  $polyline = array();
+}
+
+$route = array("routeId" => $routeid,
+               "polyline" => $polyline,
                );
 
 echo json_encode(array("route" => $route));
--- search.php
+++ search.php
@@ -13,16 +13,12 @@ function make_route($name, $description) {
                "description" => $description,
                "lastUpdate" => "1 minute ago",
                "id" => $name,
-               "polyline" => array(
-                   array(-74.0019, 40.7194),
-                   array(-74.0005, 40.7184),
-                   array(-74.0031, 40.7156),
-                   ),
                );
 }
 
 $results = array(
     make_route("M14D", "14th Street Crosstown via Avenue D"),
+    make_route("M21C", "Goes somewhere else"),
     make_stop("14AVEC0001"),
     make_stop("14AVEC0002"),
     );
